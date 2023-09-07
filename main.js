08d2c7c254113f418a625b0a7646db17cecf5127
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-07 13:44:13 -0600

    Agregando lógica preliniar

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 "use strict";
+let comision = 0;
+
+let productos = [
+    {
+        nombre: "Aqua",
+        precio: 200
+    },
+    {
+        nombre: "Emoción",
+        precio: 180
+    },
+    {
+        nombre: "Alegría",
+        precio: 160
+    },
+    {
+        nombre: "Frescura",
+        precio: 150
+    }
+];
+
+let vendedores = [
+  {
+    nombre: "José María",
+    edad: 26,
+    productosVendidos: 5, 
+    ids: { dni: 123456, passport: 9876543 },
+    activate: function () {
+      this.activo = true;
+    }
+  }
+];
