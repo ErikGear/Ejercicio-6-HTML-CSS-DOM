a576d481fd3edf30c96a68db5a1ba64ac5d7017c
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-07 19:24:45 -0600

    Agregando vendedores

--- main.js
+++ main.js
@@ -2,30 +2,41 @@
 let comision = 0;
 
 let productos = [
-    {
-        nombre: "Aqua",
-        precio: 200
-    },
-    {
-        nombre: "Emoción",
-        precio: 180
-    },
-    {
-        nombre: "Alegría",
-        precio: 160
-    },
-    {
-        nombre: "Frescura",
-        precio: 150
-    }
+  {
+    nombre: "Aqua",
+    precio: 200
+  },
+  {
+    nombre: "Emoción",
+    precio: 180
+  },
+  {
+    nombre: "Alegría",
+    precio: 160
+  },
+  {
+    nombre: "Frescura",
+    precio: 150
+  }
 ];
 
 let vendedores = [
   {
-    nombre: "José María",
+    nombre: "Juan",
     edad: 26,
-    productosVendidos: 5, 
-    ids: { dni: 123456, passport: 9876543 },
+    numeroProductosVendidos: 0,
+    tipoProductos: [],
+    ids: { dni: "123456", passport: "9876543"},
+    activate: function () {
+      this.activo = true;
+    }
+  },
+  {
+    nombre: "Pedro",
+    edad: 26,
+    numeroProductosVendidos: 0,
+    tipoProductos: [],
+    ids: { dni: "78910", passport: "6789805"},
     activate: function () {
       this.activo = true;
     }
